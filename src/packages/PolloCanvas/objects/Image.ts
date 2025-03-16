762a1bcb3983e27d95cd49ca963c0a8c11744aaf
rexleimo <rainbowmorel@163.com>
2025-03-16 16:46:23 +0800

    feat: 增强 PolloImage 类的图像交互功能，添加鼠标悬停和选中时的边框样式及控制点设置，提升用户体验

--- src/packages/PolloCanvas/objects/Image.ts
+++ src/packages/PolloCanvas/objects/Image.ts
@@ -1,27 +1,67 @@
 import { FabricImage } from "fabric";
 import cuid from "cuid";
-class PolloImage  {
+class PolloImage {
 
     private image!: FabricImage;
     private uuid: string;
 
-    constructor() { 
+    constructor() {
         this.uuid = cuid();
     }
 
     public setImage(src: string) {
-        return FabricImage.fromURL(src).then(img=>{
+        return FabricImage.fromURL(src).then(img => {
             this.image = img;
             this.image.hoverCursor = 'default';
 
             this.image.on('mouseover', () => {
-                // 是否选中态
-                const isSelected = this.image.activeOn;
-                if(isSelected){
-                    this.image.hoverCursor = 'move';
-                }else{
-                    this.image.hoverCursor = 'default';
-                }
+                this.image.hoverCursor = 'default';
+                // 绘制边框
+                this.image.set({
+                    stroke: '#00a8ff',
+                    strokeWidth: 2,
+                    strokeUniform: true,
+                });
+                this.image.canvas?.requestRenderAll();
+            });
+            this.image.on('mouseout', () => {
+                this.image.hoverCursor = 'default';
+                // 移除边框
+                this.image.set({
+                    stroke: undefined,
+                    strokeUniform: true
+                });
+                this.image.canvas?.requestRenderAll();
+            });
+
+            // 设置选中时的控制点和边框样式
+            this.image.set({
+                transparentCorners: false,
+                borderColor: '#00a8ff',
+                borderWidth: 2,
+                showControls: true,
+                hasControls: true,
+                hasBorders: true,
+                // 隐藏旋转控制点
+                hasRotatingPoint: false,
+                cornerColor: '#00a8ff',
+                // 设置控制点样式
+                cornerStyle: 'circle',
+                cornerSize: 10,
+            });
+
+            // 隐藏中间的控制点，只保留四个角的控制点
+            this.image.setControlsVisibility({
+                mt: false,  // 中上
+                mb: false,  // 中下
+                ml: false,  // 中左
+                mr: false,  // 中右
+                mtr: false,
+                // 保留四个角的控制点
+                tl: true,   // 左上
+                tr: true,   // 右上
+                bl: true,   // 左下
+                br: true    // 右下
             });
 
         });
